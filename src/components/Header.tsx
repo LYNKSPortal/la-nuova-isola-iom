6da7e1a8b28fb4f3fadd9c14677cad5a1da99a54
User <user@Mac.lan>
2026-03-28 23:31:24 +0000

    Remove and delete contact page as it's unnecessary

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -47,11 +47,6 @@ export default function Header() {
                 </Link>
               </li>
               <li>
-                <Link href="/contact" className="text-white hover:text-gray-200 px-3 py-2 text-sm font-medium transition-colors">
-                  Contact
-                </Link>
-              </li>
-              <li>
                 {/* Book Now Button */}
                 <Link 
                   href="/reservations" 
@@ -128,15 +123,6 @@ export default function Header() {
                     onClick={() => setIsMenuOpen(false)}
                   >
                     Gallery
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/contact"
-                    className="block px-3 py-2 text-base font-medium text-white hover:text-gray-200 hover:bg-white/10"
-                    onClick={() => setIsMenuOpen(false)}
-                  >
-                    Contact
                   </Link>
                 </li>
               </ul>
