2c22ddab891376ae2e2bb394caf7e409ac721d0a
User <user@Mac.lan>
2026-06-02 22:16:48 +0100

    Change PDF download section background to white on all menu pages

--- src/app/drinks-menu/page.tsx
+++ src/app/drinks-menu/page.tsx
@@ -422,7 +422,7 @@ export default function DrinksMenu() {
       </section>
 
       {/* PDF Download Section */}
-      <section className="py-8 md:py-12 bg-gray-50">
+      <section className="py-8 md:py-12 bg-white">
         <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
           <FadeIn>
             <div className="text-center">
